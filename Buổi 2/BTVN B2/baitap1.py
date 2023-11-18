7e34c0f50d543a8d066f5e79d964c22e23b3b041
MaiNgocChau9 <aurora410014@outlook.com>
2023-11-18 22:33:27 +0700

    bt1

---
+++ Buổi 2/BTVN B2/baitap1.py
@@ -0,0 +1,10 @@
+class VatNuoi:
+    Giong="Ngựa thuẩn chủng"
+    MauSac="Nâu"
+    Tuoi=12
+    CanNang=450
+a = VatNuoi()
+print("Giống:",a.Giong)
+print("Màu sắc:",a.MauSac)
+print("Tuổi:",a.Tuoi)
+print("Cân nặng:",a.CanNang)
