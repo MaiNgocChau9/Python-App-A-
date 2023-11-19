dcd79a687c89ce6a489efab775045578427ee53c
MaiNgocChau9 <chauaurora9@gmail.com>
2023-11-19 15:45:38 +0700

    Commit

--- Buổi 3/bai1.py
+++ Buổi 3/bai1.py
@@ -6,6 +6,14 @@ class PhuongTien:
     SoBanhXe = 4
     GiaTien = 22299000000
 
+    def XuatThongTin(self):
+        print("Loại xe:", self.LoaiXe)
+        print("Hãng xe:", self.HangXe)
+        print("Màu sắc:", self.MauSac)
+        print("Số chỗ ngồi:", self.SoChoNgoi)
+        print("Số bánh xe:", self.SoBanhXe)
+        print("Giá tiền:", self.GiaTien)
+
 xe = PhuongTien()
 xe.LoaiXe = "Model S Long Range"
 xe.HangXe = "Tesla"
@@ -13,3 +21,4 @@ xe.MauSac = "Đen"
 xe.SoChoNgoi = 4
 xe.SoBanhXe = 4
 xe.GiaTien = 18499000000
+xe.XuatThongTin()
