dd0346c0ded6a8fd57bc37aa34590a6f2f955c5f
MaiNgocChau9 <chauaurora9@gmail.com>
2023-11-19 15:02:23 +0700

    commit

--- Buổi 2/BTVN B2/baitap1.py
+++ Buổi 2/BTVN B2/baitap1.py
@@ -4,6 +4,18 @@ class VatNuoi:
     Tuoi=12
     CanNang=450
 a = VatNuoi()
+b = VatNuoi()
+
+a.Giong = "Mèo"
+a.MauSac = "Vàng"
+a.Tuoi = 5
+a.CanNang = 3
+
+b.Giong = "Chó"
+b.MauSac = "Đen"
+b.Tuoi = 7
+b.CanNang = 8
+
 print("Giống:",a.Giong)
 print("Màu sắc:",a.MauSac)
 print("Tuổi:",a.Tuoi)
