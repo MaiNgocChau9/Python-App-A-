2c9e50f1ff578da2e6889a32e776ec7d6a5b2b75
MaiNgocChau9 <chauaurora9@gmail.com>
2023-11-19 15:36:22 +0700

    Commit

---
+++ Buổi 3/bai1.py
@@ -0,0 +1,15 @@
+class PhuongTien:
+    LoaiXe = "Tesla Model S Plaid"
+    HangXe = "Tesla"
+    MauSac = "Xám"
+    SoChoNgoi = 4
+    SoBanhXe = 4
+    GiaTien = 22299000000
+
+xe = PhuongTien()
+xe.LoaiXe = "Model S Long Range"
+xe.HangXe = "Tesla"
+xe.MauSac = "Đen"
+xe.SoChoNgoi = 4
+xe.SoBanhXe = 4
+xe.GiaTien = 18499000000
