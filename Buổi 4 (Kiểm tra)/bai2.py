16abc87310775cf19ecf79d9a5f118be571ac214
MaiNgocChau9 <aurora410014@outlook.com>
2023-11-26 15:20:00 +0700

    commit

--- Buổi 4 (Kiểm tra)/bai2.py
+++ Buổi 4 (Kiểm tra)/bai2.py
@@ -12,14 +12,19 @@ class HocSinh():
         print("Cân nặng:", self.CanNang)
         print("Học lực:", self.HocLuc)
 #1. Khởi tạo để tạo đối tượng học sinh với tên, địa chỉ, chiều cao và cân nặng được cung cấp.
+print("#1. Khởi tạo để tạo đối tượng học sinh với tên, địa chỉ, chiều cao và cân nặng được cung cấp.")
 hocsinh = HocSinh("Mai Ngọc Châu", "Đồng Nai, Việt Nam", 158, 40, "Xuất sắc") #Cân nặng và Chiều cao có thể sai :vvv
+hocsinh.Output()
 
 # 2. Cập nhật địa chỉ mới khi học sinh chuyển nhà.
-hocsinh.DiaChi = "Mỹ (Đình), Việt Nam"
+print("\n# 2. Cập nhật địa chỉ mới khi học sinh chuyển nhà.")
+hocsinh.DiaChi = str(input("Nhập địa chỉ mới: "))
 
 #3. Cập nhật chiều cao và cân nặng mới cho học sinh khi đến kỳ khám sức khỏe.
-hocsinh.ChieuCao = 159
-hocsinh.CanNang = 40.5
+print("\n#3. Cập nhật chiều cao và cân nặng mới cho học sinh khi đến kỳ khám sức khỏe.")
+hocsinh.ChieuCao = float(input("Nhập chiều cao: "))
+hocsinh.CanNang = float(input("Nhập cân nặng: "))
 
 #4. Xuất ra thông tin của học sinh
+print("\n#4. Xuất ra thông tin của học sinh")
 hocsinh.Output()
