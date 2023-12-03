332676dc3f40ca93bb4b34993c3ec46594c4e366
Mai Ngọc Châu <chauaurora9@gmail.com>
2023-12-03 16:05:58 +0700

    commit

--- Buổi 5/bai1.py
+++ Buổi 5/bai1.py
@@ -1,6 +1,8 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
 import sys
 app = QApplication(sys.argv)
 window = QWidget()
+button = QPushButton("OK", window)
+button.setGeometry(200,200, 100, 50)
 window.show()
 app.exec()
