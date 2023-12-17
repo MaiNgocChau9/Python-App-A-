3c52d5a7b933d2dc8b4b648f271139cad3282932
MaiNgocChau9 <chauaurora9@gmail.com>
2023-12-17 15:32:47 +0700

    commit

--- Buổi 6/bai1.py
+++ Buổi 6/bai1.py
@@ -1,1 +1,13 @@
-print("Hello World")
+from PyQt6.QtWidgets import QApplication, QMainWindow
+import sys
+from PyQt6 import uic
+
+class Login(QMainWindow):
+    def __init__ (self):
+        super().__init__()
+        uic.loadUi("D:\Aurora\Python\Python App (A)\Python-App-A-\Buổi 6\Login.ui", self)
+                   
+app = QApplication(sys.argv)
+my_login_ui = Login()
+my_login_ui.show()
+app.exec()
