c64737f070f7f80233ba8790d630d177e569499f
MaiNgocChau9 <chauaurora9@gmail.com>
2024-01-20 17:15:06 +0700

    commit

--- SPCK/read.py
+++ SPCK/read.py
@@ -2,6 +2,6 @@ import os
 
 files = os.listdir("SPCK\\All Notes")
 for file in files: 
-    print(file)
+    print("Tên ghi chú:",file)
     file_path = f"SPCK\\All Notes\\{file}"
-    with open(file_path, 'r') as file: print(file.read())
+    with open(file_path, 'r') as file: print("Nội dung:",file.read())
