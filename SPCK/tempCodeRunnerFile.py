c589571c271c8c97cc5e79d640d7d9f6eb271017
MaiNgocChau9 <chauaurora9@gmail.com>
2024-01-21 14:10:25 +0700

    commit

--- SPCK/tempCodeRunnerFile.py
+++ SPCK/tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-app = QApplication(sys.argv)
-# Các cửa sổ
-login_ui = Login()
-register_ui = Register()
-home_ui = Home()
-notes_ui = Notes()
-chat_ui = Chat()
-about_ui = About()
-
-# Cửa sổ thực hiện
-home_ui.show()
-app.exec()
+)
