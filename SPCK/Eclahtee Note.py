3220bf36526f6c06ddad12d6fbb12582002e3dc8
MaiNgocChau9 <chauaurora9@gmail.com>
2024-01-21 10:59:12 +0700

    commit

--- SPCK/Eclahtee Note.py
+++ SPCK/Eclahtee Note.py
@@ -104,16 +104,6 @@ class Home(QMainWindow):
         font_button.setBold(True)
         
         # UI
-        self.textBrowser.setMarkdown("""
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
-<html><head><meta name="qrichtext" content="1" /><style type="text/css">
-p, li { white-space: pre-wrap; }
-</style></head><body style=" font-family:'Segoe UI'; font-size:8.25pt; font-weight:400; font-style:normal;">
-<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Segoe UI ';"><br /></p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-family:'Segoe UI '; font-size:16pt; font-weight:600;">Vasper Jance</span></p>
-<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Segoe UI '; font-size:8pt; font-weight:600;"><br /></p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-family:'Segoe UI '; font-size:14pt;">Welcome back, we've missed your smiling face!</span></p></body></html>
-        """)
         self.label_9.setFont(font)
         self.label_11.setFont(font)
         self.pushButton_6.setFont(font_button)
--- SPCK/tempCodeRunnerFile.py
+++ SPCK/tempCodeRunnerFile.py
@@ -1,11 +1,10 @@
-    print("Something very cool",task)
-                print("Item:",item.text())
-                check_state = item.checkState()
-                if check_state == QtCore.Qt.CheckState.Unchecked:
-                    local_item.append(task)
-                    print(local_item)
-                if check_state == QtCore.Qt.CheckState.Checked:
-                    local_item.remove(task)
-                    print(local_item)
-                if task == local_item[-1]:
-                    break
+        self.textBrowser.setMarkdown("""
+# <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
+# <html><head><meta name="qrichtext" content="1" /><style type="text/css">
+# p, li { white-space: pre-wrap; }
+# </style></head><body style=" font-family:'Segoe UI'; font-size:8.25pt; font-weight:400; font-style:normal;">
+# <p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Segoe UI ';"><br /></p>
+# <p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-family:'Segoe UI '; font-size:16pt; font-weight:600;">Vasper Jance</span></p>
+# <p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Segoe UI '; font-size:8pt; font-weight:600;"><br /></p>
+# <p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-family:'Segoe UI '; font-size:14pt;">Welcome back, we've missed your smiling face!</span></p></body></html>
+#         """)
