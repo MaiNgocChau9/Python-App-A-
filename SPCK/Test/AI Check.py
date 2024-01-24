bbb26bcac5bb2b06e3b3131526025803e18e356e
MaiNgocChau9 <chauaurora9@gmail.com>
2024-01-24 15:36:22 +0700

    commit

--- SPCK/Test/AI Check.py
+++ SPCK/Test/AI Check.py
@@ -7,7 +7,7 @@ Top_k = Mức độ chi tiết
 import google.generativeai as genai
 user = "Câu hỏi:" + str(input("User: "))
 genai.configure(api_key="AIzaSyDf_CTLM3mIPCx5n7fmNAtEQW5QeT2jgI0")
-generation_config = {"temperature": 1,"top_p": 1,"top_k": 1,"max_output_tokens": 100,}
+generation_config = {"temperature": 0.5,"top_p": 1,"top_k": 1,"max_output_tokens": 100,}
 # safety_settings = [{"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},{"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_MEDIUM_AND_ABOVE"},{"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},{"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},]
 model = genai.GenerativeModel(model_name="gemini-pro",generation_config=generation_config) # ,safety_settings=safety_settings
 # prompt_parts = ["System: Nếu để trả lời câu hỏi 1 cách chính xác mà cần đến việc đọc tất cả nội dung ghi chú hãy trả lời \"true\", không thì trả lời \"false\"", "Câu hỏi:" + str(input("User: ")),]
