4104d2d05157d2992252e7cad92c726cf1b8dc0b
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-02-19 22:47:50 +0700

    commit

--- SPCK/Windows/Test/AI model.py
+++ SPCK/Windows/Test/AI model.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 genai.configure(api_key="AIzaSyDf_CTLM3mIPCx5n7fmNAtEQW5QeT2jgI0")
 
 # Set up the model
-generation_config = {"temperature": 0.9,"top_p": 1,"top_k": 1,"max_output_tokens": 100000,}
+generation_config = {"temperature": 0.9,"top_p": 1,"top_k": 1,"max_output_tokens": 100000}
 
 model = genai.GenerativeModel(model_name="gemini-1.0-pro", generation_config=generation_config)
 
