3efe4ac86850d8fe27185e2fb79589902d9c8e92
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-02-19 22:48:56 +0700

    commit

--- SPCK/Windows/Test/AI model.py
+++ SPCK/Windows/Test/AI model.py
@@ -4,10 +4,9 @@ genai.configure(api_key="AIzaSyDf_CTLM3mIPCx5n7fmNAtEQW5QeT2jgI0")
 
 # Set up the model
 generation_config = {"temperature": 0.9,"top_p": 1,"top_k": 1,"max_output_tokens": 100000}
-
 model = genai.GenerativeModel(model_name="gemini-1.0-pro", generation_config=generation_config)
+chat = model.start_chat(history=[])
 
-convo = model.start_chat(history=[])
 while True:
-    convo.send_message(input(">>> "))
-    print(convo.last.text)
+    chat.send_message(input(">>> "))
+    print(chat.last.text)
